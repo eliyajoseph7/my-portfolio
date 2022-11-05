142fa440c00c85509788369862c2a21b00358321
eliyajoseph7 <exaveryeliya20@gmail.com>
2022-11-05 11:05:19 +0300

    initial commit

---
+++ resources/views/layouts/frontend/components/nav.blade.php
@@ -0,0 +1,28 @@
+<nav class="bg-white h-14 shadow-sm mb-4 sticky">
+    <div class="container mx-auto grid grid-flow-col grid-cols-3 gap-4 h-full px-2">
+        <div class="colspan-2 ">
+            <a class="inline-flex items-center bg-blue-700 hover:bg-blue-800 text-white font-bold my-2 px-2 focus:ring-blue-700 cursor-pointer rounded-md text-4xl">
+                iJn
+            </a>
+        </div>
+        <div class="colspan-1 inline-flex  col-end-12 items-center">
+            <a href="#" class="inline-flex items-center px-1">
+                <i class="fab fa-linkedin  text-blue-700 hover:text-blue-800 fa-xl"></i> 
+                <span class="hidden md:flex px-2 text-lg">Linkedin </span>
+            </a>
+            <a href="#" class="inline-flex items-center px-1">
+                <i class="fab fa-facebook  text-blue-700 hover:text-blue-800 fa-xl "></i> 
+                <span class="hidden md:flex px-2 text-lg">Facebook</span>
+            </a>
+            <a href="#" class="inline-flex items-center px-1">
+                <i class="fab fa-instagram  text-blue-700 hover:text-blue-800 fa-xl "></i> 
+                <span class="hidden md:flex px-2 text-lg">Instagram</span>
+            </a>
+            <a href="#" class="inline-flex items-center px-1">
+                <i class="fab fa-github  text-blue-700 hover:text-blue-800 fa-xl "></i> 
+                <span class="hidden md:flex px-2 text-lg">GitHub</span>
+            </a>
+        </div>
+
+    </div>
+</nav>
