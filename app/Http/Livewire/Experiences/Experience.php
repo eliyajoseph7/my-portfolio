d1ffbf6894d470fbf1957f004b405269548f5cea
eliyajoseph7 <exaveryeliya20@gmail.com>
2023-01-23 16:55:05 +0300

    initial commit

---
+++ app/Http/Livewire/Experiences/Experience.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Livewire\Experiences;
+
+use App\Models\Experience as ModelsExperience;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class Experience extends Component
+{
+    public $experience;
+    public $title = '';
+    public $location = '';
+    public $company = '';
+    public $start = '';
+    public $end = 'Present';
+    public $achievements = [];
+    public $experiences = [];
+
+    public $showEnd = 'hidden';
+    public $toPresent = 'checked';
+
+    public $achievementRows = [];
+    public $achievementLenght = 0;
+    protected $listeners = [
+        'removeRow'
+    ];
+
+    protected $rules = [
+        'title' => 'required',
+        'location' => 'required',
+        'company' => 'required',
+        'start' => 'required'
+    ];
+
+    // public function setStart($val) {
+    //     dump($val);
+    // }
+
+    public function mount()
+    {
+        // app('App\Http\Livewire\ProjectAndSkill')->skills();
+        $this->experience = 'hidden';
+    }
+
+    public function render()
+    {
+        if(auth()->check()) {
+            $this->experiences = ModelsExperience::with('achievements')->where('user_id', auth()->user()->id)->get();
+        } else {
+            $user = User::where('role_id', 1)->first();
+            $this->experiences = ModelsExperience::with('achievements')->where('user_id', $user->id)->get();
+        }
+        return view('livewire.experiences.experience');
+    }
+
+    public function showAddExperience()
+    {
+        $this->experience = '';
+    }
+
+    public function hideAddExperience()
+    {
+        $this->experience = 'hidden';
+    }
+
+    public function toggleEndDate()
+    {
+        if ($this->showEnd == 'hidden') {
+            $this->showEnd = 'flex';
+        } else {
+            $this->showEnd = 'hidden';
+            $this->end = 'Present';
+        }
+    }
+
+
+
+    public function addAchievementRow()
+    {
+        $length = $this->achievementLenght;
+        $new = '<div class="d-flex items-center justify-between grid grid-flow-col mb-3" id="row' . $length . '">
+                    <div class="col-span-10">
+                        <textarea type="text" rows="1" wire:model="achievements.' . rand() . '" name="achievements[]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="..........."> </textarea>
+                    </div>
+                    <div class="float-right col-span-1 pr-1">
+                        <a row="' . $length . '" class="remove text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md px-2 py-1"><i class="fa fa-minus"></i></a>
+                    </div>
+                </div>';
+        if ($length < 5) {
+            array_push($this->achievementRows, $new);
+            $this->achievementLenght++;
+        } else {
+            $this->emit('maximum_achievements');
+        }
+    }
+
+    public function removeRow($row)
+    {
+        unset($this->achievementRows[$row]);
+        $this->achievementLenght--;
+    }
+
+    public function addExperience($formData)
+    {
+        $this->start = $formData['start'];
+        $this->validate();
+        if($this->end == '') {
+            $this->end = 'Present';
+        }
+        $expr = ModelsExperience::create(
+            [
+                'title' => $this->title,
+                'company' => $this->company,
+                'location' => $this->location,
+                'from' => $this->start,
+                'to' => $this->end,
+                'user_id' => auth()->user()->id
+            ]
+        );
+
+        $data = [];
+        foreach($this->achievements as $key=>$value) {
+            $data[] = [
+                'achievement' => $value,
+                'experience_id' => $expr->id,
+                'created_at' => now(),
+                'updated_at' => now()
+            ];
+        }
+
+        DB::table('achievements')->insert($data);
+        
+        session()->flash('feedback', 'Experience successfully added.');
+        $this->reset(['title', 'company', 'location', 'start', 'achievements', 'end', 'achievementRows']);
+    }
+}
---
+++ resources/views/livewire/experiences/experience.blade.php
@@ -0,0 +1,39 @@
+<div class="border bg-white shadow-sm rounded-lg border-b-slate-200">
+    <div class="p-5">
+        @if (auth()->check())
+        <div class="float-right">
+            <i class="fa fa-plus-circle fa-2xl cursor-pointer text-blue-400 hover:text-blue-800 p-3 relative " aria-labelledby="add-experience" role="dialog" aria-modal="true" wire:click="showAddExperience()"></i>
+        </div>
+
+        @endif
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity {{$experience}} " wire:click="hideAddExperience()"></div>
+
+        @include('livewire.experiences.add_experience')
+        <h3 class="text-gray-800 py-1 hover:text-gray-900 tracking-tight cursor-pointer text-3xl font-bold dark:text-white font-mono">Work Experience</h3>
+        <hr>
+        <div class="w-full">
+            @forelse ($experiences as $exp)
+            <h1 class="text-3xl font-serif italic w-96 md:w-full"><i class="fa fa-star text-gray-500"></i> {{$exp->title}} - {{ $exp->company }}</h1>
+            <a class="py-1 pl-10 text-lg w-96 md:w-full">{{ $exp->location }} {{ $exp->from }} - {{ $exp->to }}</a>
+            <a class="py-1 pl-10 text-lg w-full"></a>
+
+            <h2 class="pl-10 mb-2 text-lg font-semibold italic text-gray-900 dark:text-white w-full">Achievements:</h2>
+            <ul class="space-y-1 p-5 list-inside text-gray-500 dark:text-gray-400 w-96 md:w-full">
+                @forelse ($exp->achievements as $task)
+                <li class="flex items-center pl-5">
+                    <i class="fa fa-check-circle w-4 h-4 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0 " fill="currentColor"></i>
+                    {{ $task->achievement }}
+                </li>
+
+                @empty
+                    <p>No any achievement</p>
+                @endforelse
+
+            </ul>
+
+            @empty
+                <p>No any experience</p>
+            @endforelse
+        </div>
+    </div>
+</div>
